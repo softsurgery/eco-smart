a5240651c43612e900c55fa5c79c3028f34fd19c
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2026-02-07 15:33:12 +0100

    feat(main-window): add employee window redirection (slot initial)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "employee.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , employeeWindow(nullptr)
 {
     ui->setupUi(this);
+    connect(ui->redirect, &QPushButton::clicked, this, &MainWindow::onDashboardButtonClicked);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete employeeWindow;
+}
+
+void MainWindow::onDashboardButtonClicked()
+{
+    if (employeeWindow == nullptr) {
+        employeeWindow = new Employee();
+    }
+    employeeWindow->show();
+    this->close();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+class Employee;
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -17,7 +19,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onDashboardButtonClicked();
+
 private:
     Ui::MainWindow *ui;
+    Employee *employeeWindow;
 };
 #endif // MAINWINDOW_H
